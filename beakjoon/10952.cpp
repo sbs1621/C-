c0b3d2272266de61b8182459e46ba39df16bca29
sbs1621 <80839419+sbs1621@users.noreply.github.com>
2021-03-25 22:25:59 +0900

    Create 10952.cpp

---
+++ beakjoon/10952.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a, b;
+		int c = 0;
+	while (true)
+	{
+		cin >> a >> b;
+		if (0 < a && b < 10)
+		{
+			
+			cout << a + b << endl;
+		}
+		else
+			break;
+		
+	}
+	
+}
