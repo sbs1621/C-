e97e1e023699e5d3983c2d7f6b22d592f77dc29c
BSS <80839419+sbs1621@users.noreply.github.com>
2021-04-01 21:31:54 +0900

    Update 1_1번.cpp

--- 명품C++/1_1번.cpp
+++ 명품C++/1_1번.cpp
@@ -6,10 +6,15 @@ int main()
 	int c = 1;
 	while (true)
 	{
-		if(c %10 == 0)
+		if (c % 10 == 0)
 			cout << c << endl;
 		else
+		{
+			if(c > 9)
 			cout << c << " ";
+			else
+				cout << c << "  ";
+		}
 		c++;
 		if(c == 101)
 			break;
